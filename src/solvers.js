68ca5c1eebdac5d042cd369ca6a73e5067ff4e81
millionhari <mikemhari@gmail.com>
2015-02-13 00:09:59 -0800

    Finish countNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -54,6 +54,7 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
+
   var solution = undefined; //fixme
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
@@ -63,7 +64,40 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // make a new board
+  // make a solutionCount
+  // make currentRow
+  // get rows of board
+  // create an inner function
+    // if you reach the last row,
+      // solutioncount++
+    // loop through the row, then toggle the element from 0 to 1
+      // check if the toggle'd element has any conflicts
+      // if there are no conflicts
+        // call recursion on currentRow++ (array)
+      // toggle back from 1 to 0
+
+
+  var board = new Board({n:n});
+  var solutionCount = 0;
+
+
+  var recurseNQueens = function(rowIndex){
+    if (rowIndex === n){
+      solutionCount++;
+      return;
+    }
+    for (var i = 0; i < n; i++){
+      board.togglePiece(rowIndex, i);
+      // if (!board.hasAnyQueensConflicts()) {
+      if (!board.hasAnyQueenConflictsOn(rowIndex, i)) {
+        recurseNQueens(rowIndex+1);
+      }
+      board.togglePiece(rowIndex, i);
+    }
+  };
+
+  recurseNQueens(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
